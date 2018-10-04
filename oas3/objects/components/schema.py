e0b8b3bceab9468b80229bfd5ce6ae4f8abd98e7
David Westerhoff <dmwesterhoff@gmail.com>
2018-10-04 10:57:20 -0700

    Fixed schemas to return commonly used attributes

--- oas3/objects/components/schema.py
+++ oas3/objects/components/schema.py
@@ -9,19 +9,56 @@ from oas3.base import BaseObject, BaseSchema
 
 class Schema(BaseObject):
     """
-    Holds a set of reusable objects for different aspects of the OAS.
-    All objects defined within the components object will have no effect on the API
-    unless they are explicitly referenced from properties outside the components object.
+    The Schema Object allows the definition of input and output data types.
+    These types can be objects, but also primitives and arrays. This object is
+    an extended subset of the JSON Schema Specification Wright Draft 00.
 
     .. note:
-        https://github.com/OAI/OpenAPI-Specification/blob/master/versions/3.0.0.md#componentsObject
+        https://github.com/OAI/OpenAPI-Specification/blob/master/versions/3.0.0.md#schemaObject
     """
 
     class Schema(BaseSchema):
-        pass
+        #title =
+        #multiple_of =
+        #maximum =
+        #exclusive_maximum =
+        #minimum =
+        #exclusive_minimum =
+        #max_length =
+        #min_length =
+        #pattern =
+        #max_items =
+        #min_items =
+        #unique_items =
+        #max_properties =
+        #min_properties =
+        required = fields.List(fields.Str)
+        #enum =
+        schema_type = fields.Str(load_from='type', dump_to='type')
+        all_of = fields.List(fields.Raw, load_from='allOf', dump_to='allOf')
+        #one_of =
+        #any_of =
+        #not_of =
+        #items =
+        properties = fields.Dict()
+        #additional_properties =
+        #description =
+        #schema_format =
+        #default =
+        #nullable =
+        #read_only =
+        #write_only =
+        #xml =
+        #external_docs =
+        example = fields.Dict()
+        #deprecated =
 
         def represents(self):
             return Schema
 
-    def __init__(self, schemas=None):
-        self.schemas = schemas
+    def __init__(self, properties=None, required=None, schema_type=None, all_of=None, example=None):
+        self.properties = properties
+        self.required = required
+        self.schema_type = schema_type
+        self.all_of = all_of
+        self.example = example
